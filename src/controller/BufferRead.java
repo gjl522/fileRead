b9e12757b2bab8ddcb5578ee9799e274e147b0f0
DELL <986174990@qq.com>
2020-10-22 16:33:55 +0800

    fileRead的第2个分支

--- src/controller/BufferRead.java
+++ src/controller/BufferRead.java
@@ -13,7 +13,7 @@ public class BufferRead {
     public static void main(String[] args) {
         //定义一个空的缓冲流
         BufferedReader buff = null;
-        System.out.println("6666666666666666666666666666666666666");
+        System.out.println("66666666666666666666666666666666666667777777777777777");
         try {
             //将具体文件通过FileReader读取出来
             FileReader fileReader = new FileReader("D:\\WorkCode\\File\\gjl.txt");
